91298e393039f2dedd2646c3d4ceadf66a5c0548
aziz200115 <awekerbekov@gmail.com>
2024-06-15 22:01:32 -0500

    updated monitoring and added variables.tf files under each folder

--- qa/monitoring.tf
+++ qa/monitoring.tf
@@ -1,5 +1,6 @@
-resource "google_monitoring_uptime_check_config" "qa-https" {
-  display_name = "https://qa-artemis.shekerbekov.com"
+resource "google_monitoring_uptime_check_config" "https" {
+  project      = var.project_id
+  display_name = "https://${var.service_name}.${var.google_domain_name}"
   timeout      = "60s"
 
   http_check {
@@ -12,8 +13,8 @@ resource "google_monitoring_uptime_check_config" "qa-https" {
   monitored_resource {
     type = "uptime_url"
     labels = {
-      project_id = "terraform-project-aziz-423316"
-      host       = "qa-artemis.shekerbekov.com"
+      project_id = var.project_id
+      host       = "${var.service_name}.${var.google_domain_name}"
     }
   }
 }
---
+++ stage/variables.tf
@@ -0,0 +1,17 @@
+variable "google_domain_name" {
+  description = "Please provide a google_domain_name"
+  type        = string
+  default     = "shekerbekov.com"
+}
+
+variable "service_name" {
+  description = "Please provide a service_name"
+  type        = string
+  default     = "stage-artemis"
+}
+
+variable "project_id" {
+  description = "Please provide a project ID"
+  type        = string
+  default     = "terraform-project-aziz-423316"
+}
